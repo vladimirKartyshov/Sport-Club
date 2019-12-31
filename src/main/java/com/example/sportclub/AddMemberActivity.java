bbbf4f1a22144ebf33fd0a788c123fe672934060
vladimirKartyshov <vovan198080@inbox.ru>
2019-12-31 16:28:47 +0300

    ArrayAdapter

--- src/main/java/com/example/sportclub/AddMemberActivity.java
+++ src/main/java/com/example/sportclub/AddMemberActivity.java
@@ -1,10 +1,13 @@
 package com.example.sportclub;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class AddMemberActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class AddMemberActivity extends AppCompatActivity {
     private EditText groupEditText;
     private Spinner genderSpinner;
     private int gender = 0;
+    private ArrayAdapter spinnerAdapter;
+    private ArrayList spinnerArraylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,33 @@ public class AddMemberActivity extends AppCompatActivity {
         lastNameEditText = findViewById(R.id.lastNameEditText);
         groupEditText = findViewById(R.id.groupEditText);
         genderSpinner = findViewById(R.id.genderSpinner);
+
+        //это динамический метод тк мы можем сколько угодно добавлять эл-ов в arraylist
+        // те опций spinner.При фиксиров-м кол-ве исп-ся статический способ
+
+//        spinnerArraylist = new ArrayList();
+//        spinnerArraylist.add("Unknow");
+//        spinnerArraylist.add("Male");
+//        spinnerArraylist.add("Female");
+//
+//        spinnerAdapter = new ArrayAdapter(this,
+//                android.R.layout.simple_spinner_item, spinnerArraylist);
+//        // создаем вид каждого эл-та spinner
+//        spinnerAdapter.setDropDownViewResource(
+//                android.R.layout.simple_spinner_dropdown_item);
+//        //устанавливаем нашему спинеру этот адаптор
+//        genderSpinner.setAdapter(spinnerAdapter);
+
+         //===============================================================
+         //===============================================================
+
+        //статический способ, где кол-во эл-в фиксировано в спинаре
+        // для начала создается нов ресурс файл arrays в values
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
     }
 }

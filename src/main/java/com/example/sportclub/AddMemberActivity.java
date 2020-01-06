6240f564897445a4924f2be732255cdb567e9380
vladimirKartyshov <vovan198080@inbox.ru>
2020-01-06 06:56:00 +0300

    add and delete (menu)

--- src/main/java/com/example/sportclub/AddMemberActivity.java
+++ src/main/java/com/example/sportclub/AddMemberActivity.java
@@ -1,11 +1,18 @@
 package com.example.sportclub;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import java.util.ArrayList;
 
@@ -56,5 +63,45 @@ public class AddMemberActivity extends AppCompatActivity {
         spinnerAdapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(spinnerAdapter);
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String selectedGender = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    if (selectedGender.equals("Male")) {
+                        gender = 1;
+                    } else if (selectedGender.equals("Female")) {
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                gender = 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

a2a29b1c85a67be9c4437cd693d6b70c079c2c3c
vladimirKartyshov <vovan198080@inbox.ru>
2020-01-12 09:03:41 +0300

    UriMatcher

--- src/main/java/com/example/sportclub/db/SportContentProvider.java
+++ src/main/java/com/example/sportclub/db/SportContentProvider.java
@@ -2,12 +2,28 @@ package com.example.sportclub.db;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class SportContentProvider extends ContentProvider {
 
     SportDbOpenHelper sportDbOpenHelper;
+
+    private static final int MEMBERS = 111;
+    private static final int MEMBER_ID = 222;
+
+    // Creates a UriMatcher object.
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // в статическом блоке код срабатывает без создания обЪекта класса (content provider)
+    static {
+        // для работы со всей таблицей
+        uriMatcher.addURI(ClubSportContract.AUTHORITY,ClubSportContract.PATH_MEMBERS, MEMBERS);
+
+        //для работы с одной строкой
+        uriMatcher.addURI(ClubSportContract.AUTHORITY,ClubSportContract.PATH_MEMBERS + "/#",MEMBER_ID);
+    }
 
     @Override
     public boolean onCreate() {

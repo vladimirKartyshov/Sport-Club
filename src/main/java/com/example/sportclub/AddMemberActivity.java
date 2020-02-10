7871ca8339b1248dd35d5a494f951f6220d0fd9d
vladimirKartyshov <vovan198080@inbox.ru>
2020-02-10 16:20:20 +0300

    Обновление данных в базе данных

--- src/main/java/com/example/sportclub/AddMemberActivity.java
+++ src/main/java/com/example/sportclub/AddMemberActivity.java
@@ -122,7 +122,7 @@ public class AddMemberActivity extends AppCompatActivity implements LoaderManage
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.save_member:
-                insertMember();
+                saveMember();
                 return true;
             case R.id.delete_member:
                 return true;
@@ -133,7 +133,7 @@ public class AddMemberActivity extends AppCompatActivity implements LoaderManage
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertMember(){
+    private void saveMember(){
 
         String firstName = firstNameEditText.getText().toString().trim();//trim-убирает пробелы в нач и конц строки
         String lastName = lastNameEditText.getText().toString().trim();
@@ -145,16 +145,28 @@ public class AddMemberActivity extends AppCompatActivity implements LoaderManage
         contentValues.put(ClubSportContract.MemberEntry.COLUMN_SPORT,sport);
         contentValues.put(ClubSportContract.MemberEntry.COLUMN_GENDER,gender);
 
-        //для того чтобы поместить contentValues должны использовать ContentResolver
-        ContentResolver contentResolver = getContentResolver();
-        Uri uri = contentResolver.insert(ClubSportContract.MemberEntry.CONTENT_URI,contentValues);
+        if (currentMemberUri == null){
 
-        if (uri==null){
-            Toast.makeText(this,"Insertion of data in the table",Toast.LENGTH_LONG).show();
+            //для того чтобы поместить contentValues должны использовать ContentResolver
+            ContentResolver contentResolver = getContentResolver();
+            Uri uri = contentResolver.insert(ClubSportContract.MemberEntry.CONTENT_URI,contentValues);
+
+            if (uri==null){
+                Toast.makeText(this,"Insertion of data in the table",Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this,"Data saved",Toast.LENGTH_LONG).show();
+            }
+
         }else {
-            Toast.makeText(this,"Data saved",Toast.LENGTH_LONG).show();
-        }
+            int rowsChanged = getContentResolver().update(currentMemberUri
+                    ,contentValues,null,null);
 
+            if (rowsChanged == 0){
+                Toast.makeText(this,"Saving of data in the table failed",Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this,"Member updated",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @NonNull
